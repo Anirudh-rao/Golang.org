f368598e2616b39412802b8073c0baebf5fff6b4
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2024-01-03 18:52:43 +0530

    Functions in Golang

---
+++ 4.Functions/Closures.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	//Closures
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
