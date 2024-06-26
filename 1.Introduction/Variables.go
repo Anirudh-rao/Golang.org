9df770f5845c55c878e47d0492afb8d7900499c5
Anirudh Rao <65554770+Anirudh-rao@users.noreply.github.com>
2024-06-26 11:23:17 +0530

    Values and Variables

--- 1.Introduction/Variables.go
+++ 1.Introduction/Variables.go
@@ -1,3 +1,21 @@
 package Introduction
 
-func main() {}
+import "fmt"
+
+func main() {
+	var a = "intial"
+	fmt.Println(a)
+
+	//We can also declare the data type
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
